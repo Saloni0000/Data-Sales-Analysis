6ead75861259dec2b5bb08361e55bc40d8b81615
saloni kushwaha <113685644+Saloni0000@users.noreply.github.com>
2025-02-21 03:03:15 +0530

    Update power.py

--- power.py
+++ power.py
@@ -4,20 +4,16 @@ import streamlit as st
 st.title("Power BI Chart in Streamlit")
 
 # Use os.path.join for cross-platform compatibility
-image_file1 = os.path.join("image", "power_page-0001.jpg")
-image_file2 = os.path.join("image", "power_page-0002.jpg")
-image_file3 = os.path.join("image", "power_page-0003.jpg")
+image_file = os.path.join("image", "power_page-0001.jpg")
+
 
 # Debugging: Print current working directory
 st.write(f"Current Directory: {os.getcwd()}")
-st.write(f"Looking for file at: {image_file1}")
-st.write(f"Looking for file at: {image_file2}")
-st.write(f"Looking for file at: {image_file3}")
+st.write(f"Looking for file at: {image_file}"
 
 # Check if file exists before displaying
-if os.path.exists(image_file1,image_file2,image_file3):
-    st.image(image_file1, caption="Power BI Report Visualization", use_container_width=True)  # Updated parameter
-    st.image(image_file2, caption="Power BI Report Visualization", use_container_width=True)  # Updated parameter
-    st.image(image_file3, caption="Power BI Report Visualization", use_container_width=True)  # Updated parameter
+if os.path.exists(image_file):
+    st.image(image_file, caption="Power BI Report Visualization", use_container_width=True)  # Updated parameter
+
 else:
     st.error(f"Error: File '{image_file}' not found! Check file path and directory.")
